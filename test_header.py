cde970cbaacfc029c6636638e7b5211bddc3532b
robin <13999868+robinbaneke@users.noreply.github.com>
2018-10-11 19:10:40 +0200

    change tests for header.py

--- test_header.py
+++ test_header.py
@@ -3,7 +3,7 @@
 """
 
 import unittest
-from header import read_header, len_header
+from header import read_header, len_header, fil_double_to_angle
 
 class TestHeader(unittest.TestCase):
     """
@@ -21,15 +21,6 @@ class TestHeader(unittest.TestCase):
         self.assertEqual(header_dict[b'data_type'], 1)
         self.assertEqual(header_dict[b'fch1'], 433.968)
 
-    def test_wrong_filetype(self):
-        """
-            When reading incorrect filtype
-            header.py should raise a RuntimeError
-        """
-        filename = './header.py'
-        with self.assertRaises(RuntimeError):
-            read_header(filename)
-
     def test_len_header(self):
         """
             When calculating header length
@@ -39,6 +30,16 @@ class TestHeader(unittest.TestCase):
         correct_header_length = 242
         header_length = len_header(filename)
         self.assertEqual(header_length, correct_header_length)
+
+    def test_fil_double_to_angle(self):
+        """
+            When calculating the angle
+            header.py should return the correct angle
+        """
+        double_value = 123000.0
+        expect_angle = 12.5
+        angle = fil_double_to_angle(double_value)
+        self.assertAlmostEqual(angle, expect_angle)
 
 if __name__ == '__main__':
     unittest.main()
