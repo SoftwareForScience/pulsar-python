4ffc21f9a44c9a633e04ecf83154a6e5a0b5ed67
Timo Hermsen <timo.hermsen@ti-more.com>
2018-10-15 13:46:53 +0200

    - Added setup.py used for the Asteria module. (PyLint safe)

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-"""
-    Setup for IO library
-"""
-from setuptools import setup
+# -*- coding: utf-8 -*-
+"""Setup configuration file. Used to describe the Asteria Package.  """
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    README = f.read()
+
+with open('LICENSE') as f:
+    LICENSE = f.read()
 
 setup(
-    name='IO',
-    version='0.1',
-    description='Utilities for reading Filterbank files'
+    name='asteria',
+    version='0.0.1',
+    description='An open-source DSP library for pulsar detection.',
+    long_description=README,
+    author='Amsterdam University of Applied Sciences',
+    author_email='',
+    url='https://github.com/AUAS-Pulsar/Asteria',
+    license=LICENSE,
+    packages=find_packages(exclude=('tests', 'docs', 'examples'))
 )
