e5a6e0397c9c97d6aa47a93ea8ad1f279bfc73e6
robin <13999868+robinbaneke@users.noreply.github.com>
2018-10-16 11:36:13 +0200

    fix pylint import issue tests

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,12 +1,13 @@
+"""
+    plot.py unit tests
+"""
+
 import unittest
 import numpy as np
 
-from context import plot
+from context import plot # pylint: disable-msg=E0611
 
 class TestPlot(unittest.TestCase):
     """
         Testclass for testing the plot class
     """
-
-    def test_psd(self):
-        
