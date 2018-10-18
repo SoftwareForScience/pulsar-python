6269fbd884660d2914f48e965c8f5330bc9fdc0b
Timo Hermsen <timo.hermsen@ti-more.com>
2018-10-18 17:04:25 +0200

    Updated context for travis compatibility.

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -5,7 +5,7 @@ import unittest
 import pytest # pylint: disable-msg=E0401
 import numpy as np
 from numpy.testing import assert_array_equal, assert_almost_equal, assert_allclose
-import plot.plot
+import context.plot.plot
 
 
 from context import plot # pylint: disable-msg=E0611
