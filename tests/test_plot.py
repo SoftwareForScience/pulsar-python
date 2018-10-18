5ff1209797a2462ad08e011b035841202af23dc5
Timo Hermsen <timo.hermsen@hva.nl>
2018-10-18 10:36:50 +0200

    Context test for TravisCI
    Signed-off-by: Timo Hermsen <timo.hermsen@ti-more.com>

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,12 +1,13 @@
 """
     plot.py unit tests
 """
-# import unittest
-# import numpy as np
+import unittest
+import numpy as np
 
-# from context import plot # pylint: disable-msg=E0611
+from tests.context import plot  # pylint: disable-msg=E0611
 
-# class TestPlot(unittest.TestCase):
-#     """
-#         Testclass for testing the plot class
-#     """
+
+class TestPlot(unittest.TestCase):
+    """
+        Testclass for testing the plot class
+    """
