44ad04cb0c7f7fda96be7eaebf552069a567abe6
Max van Hasselt <maxvanhasselt@hotmail.nl>
2018-10-29 11:13:19 +0100

    test window hanning

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -6,30 +6,30 @@ import numpy as np
 
 from context import plot # pylint: disable-msg=E0611
 
-class TestPlot(unittest.TestCase):
+class TestWindowHanning(unittest.TestCase):
     """
         Testclass for testing the plotting functions
     """
+    np.random.seed(0)
+    n = 1000
 
-    def __init__(self):
-        np.random.seed(0)
-        n = 1000
+    sig_rand = np.random.standard_normal(n) + 100.
+    sig_ones = np.ones(n)
 
-        self.sig_rand = np.random.standard_normal(n) + 100.
-        self.sig_ones = np.ones(n)
+    Fs = 100
 
     def test_window_hanning_rand(self):
         targ = np.hanning(len(self.sig_rand)) * self.sig_rand
         res = plot.window_hanning(self.sig_rand)
 
-        self.assertAlmostEquals(targ.all(), res.all())
+        self.assertAlmostEqual(targ.all(), res.all())
+
 
     def test_window_hanning_ones(self):
         targ = np.hanning(len(self.sig_ones))
         res = plot.window_hanning(self.sig_ones)
 
-        self.assertAlmostEquals(targ.all(), res.all(), atol=1e-06)
-
-
+        self.assertAlmostEqual(targ.all(), res.all())
+        
 if __name__ == '__main__':
     unittest.main()
