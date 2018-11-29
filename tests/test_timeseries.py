8b3951a5519dacecfeaa4bad8ea8c67730f4ab8f
Timo Hermsen <timo.hermsen@ti-more.com>
2018-11-29 14:35:30 +0100

    - Updated timeseries tests, mock_data is now globally available instead of one mock array per test which is inefficient.

--- tests/test_timeseries.py
+++ tests/test_timeseries.py
@@ -2,7 +2,6 @@
     test_timeseries.py file for testing the timeseries module.
 """
 
-
 import math
 import unittest
 
@@ -14,8 +13,20 @@ from timeseries.timeseries import Timeseries
 
 class TestTimeseries(unittest.TestCase):
     """
-        Testclass for testing the timeseries module.
+           Testclass for testing the timeseries module.
     """
+
+    timeseries_array = [-3.4771993, 28.411894, 1.5027914, 12.363131, -9.0473995,
+                        13.312918, -8.387265, 2.095158, 9.921608, -1.1714606,
+                        10.393141, -7.7038407, 24.99097, -15.776686, 0.53236485,
+                        -11.958438, -6.911227, 8.95864, 11.76904, -2.0692701,
+                        15.08512, -5.0073714, 0.35948455, -9.356973, -5.992894,
+                        4.0259366, -7.884355, 6.3973246, 3.8018064, 13.149004,
+                        8.768967, 5.0174375, 27.876976, -11.685825, 4.2599483,
+                        20.358032, 9.604366, -12.078389, 4.7057323, -11.94255,
+                        -17.29993, -2.19723, -9.520994, 2.9300866, 0.26252317,
+                        -18.206154, -22.393536, -3.3634825, 2.3057923, 18.17247,
+                        3.1966555, -5.1954975, 0.55871594, -1.2240806, -8.97003]
 
     def test_get_timeseries(self):
         """
@@ -24,19 +35,8 @@ class TestTimeseries(unittest.TestCase):
             the timeseries as an numpy array.
         :return:
         """
-        timeseries_array = [-3.4771993, 28.411894, 1.5027914, 12.363131, -9.0473995,
-                            13.312918, -8.387265, 2.095158, 9.921608, -1.1714606,
-                            10.393141, -7.7038407, 24.99097, -15.776686, 0.53236485,
-                            -11.958438, -6.911227, 8.95864, 11.76904, -2.0692701,
-                            15.08512, -5.0073714, 0.35948455, -9.356973, -5.992894,
-                            4.0259366, -7.884355, 6.3973246, 3.8018064, 13.149004,
-                            8.768967, 5.0174375, 27.876976, -11.685825, 4.2599483,
-                            20.358032, 9.604366, -12.078389, 4.7057323, -11.94255,
-                            -17.29993, -2.19723, -9.520994, 2.9300866, 0.26252317,
-                            -18.206154, -22.393536, -3.3634825, 2.3057923, 18.17247,
-                            3.1966555, -5.1954975, 0.55871594, -1.2240806, -8.97003]
 
-        timeseries_array = np.array(timeseries_array)
+        timeseries_array = np.array(self.timeseries_array)
         get_timeseries = Timeseries(timeseries_array).get()
 
         self.assertEqual(timeseries_array.all(), get_timeseries.all())
@@ -71,20 +71,9 @@ class TestTimeseries(unittest.TestCase):
         """
         downsample_rate_q = 3
 
-        timeseries_array = [-3.4771993, 28.411894, 1.5027914, 12.363131, -9.0473995,
-                            13.312918, -8.387265, 2.095158, 9.921608, -1.1714606,
-                            10.393141, -7.7038407, 24.99097, -15.776686, 0.53236485,
-                            -11.958438, -6.911227, 8.95864, 11.76904, -2.0692701,
-                            15.08512, -5.0073714, 0.35948455, -9.356973, -5.992894,
-                            4.0259366, -7.884355, 6.3973246, 3.8018064, 13.149004,
-                            8.768967, 5.0174375, 27.876976, -11.685825, 4.2599483,
-                            20.358032, 9.604366, -12.078389, 4.7057323, -11.94255,
-                            -17.29993, -2.19723, -9.520994, 2.9300866, 0.26252317,
-                            -18.206154, -22.393536, -3.3634825, 2.3057923, 18.17247,
-                            3.1966555, -5.1954975, 0.55871594, -1.2240806, -8.97003]
-
         # Downsample by 3 so array should be 3 times smaller.
 
-        downsamples_arr = Timeseries(timeseries_array).downsample(downsample_rate_q)
+        downsamples_arr = Timeseries(self.timeseries_array).downsample(downsample_rate_q)
 
-        self.assertEqual(len(downsamples_arr), math.ceil(len(timeseries_array) / downsample_rate_q))
+        self.assertEqual(len(downsamples_arr),
+                         math.ceil(len(self.timeseries_array) / downsample_rate_q))
