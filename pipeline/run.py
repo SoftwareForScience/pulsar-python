23cfdd92186bf614c13c4d060448320e5c46efed
robin <13999868+robinbaneke@users.noreply.github.com>
2018-12-20 17:10:03 +0100

    add newline

--- pipeline/run.py
+++ pipeline/run.py
@@ -4,9 +4,10 @@
 import os
 import pipeline
 
-# read static
-pipeline.Pipeline(filename=os.path.abspath("E:/11100335.320.all.fil"), size=49150)
-# read stream, row per row
-# pipeline.Pipeline(filename=os.path.abspath("E:/11100335.320.all.fil"), as_stream=True)
-# read stream, n rows
-# pipeline.Pipeline(filename=os.path.abspath("E:/11100335.320.all.fil"), as_stream=True, n=10)
+for i in range(10):
+    # # read static
+    pipeline.Pipeline(filename=os.path.abspath("E:/11100335.320.all.fil"), size=49150)
+    # read stream, row per row
+    pipeline.Pipeline(filename=os.path.abspath("E:/11100335.320.all.fil"), as_stream=True)
+    # read stream, n rows
+    pipeline.Pipeline(filename=os.path.abspath("E:/11100335.320.all.fil"), as_stream=True, n=10)
