73a5b5dcc11ee960411efe7548ef9f04c53bef9e
robin <13999868+robinbaneke@users.noreply.github.com>
2019-01-23 15:47:59 +0100

    add also 16 and 32 bits tests

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -20,16 +20,16 @@ class TestGenerate(unittest.TestCase):
         b'tsamp': 8e-05,
         b'period': .5,
         b'fch1': 400,
-        b'nbits': 8,
         b'nifs': 1
     }
 
-    def test_generate_fil(self):
+    def test_generate_8bit_fil(self):
         """
-            Generate a fake filterbank file
+            Generate a fake 8bit filterbank file
             and remove afterwards
         """
         filename = './pspm_test.fil'
+        self.header_dict[b'nbits'] = 8
         generate.generate_file(filename, self.header_dict)
         fil = filterbank.Filterbank(filename, read_all=True)
         _, fil_data = fil.select_data()
@@ -39,6 +39,37 @@ class TestGenerate(unittest.TestCase):
         self.assertEqual(self.header_dict[b'nchans'], len(fil_data[0]))
         os.remove(filename)
 
+    def test_generate_16bit_fil(self):
+        """
+            Generate a fake 16bit filterbank file
+            and remove afterwards
+        """
+        filename = './pspm_test.fil'
+        self.header_dict[b'nbits'] = 16
+        generate.generate_file(filename, self.header_dict)
+        fil = filterbank.Filterbank(filename, read_all=True)
+        _, fil_data = fil.select_data()
+        self.assertEqual(self.header_dict[b'telescope_id'], fil.header[b'telescope_id'])
+        self.assertEqual(self.header_dict[b'machine_id'], fil.header[b'machine_id'])
+        self.assertEqual(self.header_dict[b'nbits'], fil.header[b'nbits'])
+        self.assertEqual(self.header_dict[b'nchans'], len(fil_data[0]))
+        os.remove(filename)
+
+    def test_generate_32bit_fil(self):
+        """
+            Generate a fake 32bit filterbank file
+            and remove afterwards
+        """
+        filename = './pspm_test.fil'
+        self.header_dict[b'nbits'] = 32
+        generate.generate_file(filename, self.header_dict)
+        fil = filterbank.Filterbank(filename, read_all=True)
+        _, fil_data = fil.select_data()
+        self.assertEqual(self.header_dict[b'telescope_id'], fil.header[b'telescope_id'])
+        self.assertEqual(self.header_dict[b'machine_id'], fil.header[b'machine_id'])
+        self.assertEqual(self.header_dict[b'nbits'], fil.header[b'nbits'])
+        self.assertEqual(self.header_dict[b'nchans'], len(fil_data[0]))
+        os.remove(filename)
 
 if __name__ == '__main__':
     unittest.main()
