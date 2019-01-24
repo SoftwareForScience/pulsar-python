9c3ff7698d468a0dd180f68ebbbb19bf1f469cd7
robert <robertk111@hotmail.com>
2019-01-24 15:48:04 +0100

    Changed some hard_coded values to parameters + general changes

--- dedisperse/dedisperse.py
+++ dedisperse/dedisperse.py
@@ -7,8 +7,10 @@ import numpy as np
 def dedisperse(samples, highest_x=None, max_delay=None, dm=None):
     '''
     This method performs dedispersion on the filterbank data
-    The maximum_delay specifies between the currently considered pulsar signal and the next pulsar signal should be
-    The highest_x specifies the amount of intensities that are used for estimating the minimum pulsar intensity
+    The maximum_delay specifies between the currently considered pulsar signal and the next pulsar
+    signal should be
+    The highest_x specifies the amount of intensities that are used for estimating the minimum
+    pulsar intensity
     '''
 
     # Check if parameters contain a DM, if not, estimate one
@@ -42,15 +44,16 @@ def find_dm(samples, pulsar_intensity, max_delay):
     # Loop through the samples to find a pulsar intensity to start calculating from
     for s, sample in enumerate(samples[:, 0]):
 
-        # If the sample meets the minimum intensity, attempt to find a line continuing from this intensity
-        if(sample > pulsar_intensity):
+        # If the sample meets the minimum intensity, attempt to find a line continuing from
+        # this intensity
+        if sample > pulsar_intensity:
             start_sample_index = s
 
-            # Attempt to find a line, line_coordinates contains the first and last index of the pulsar
+            # Attempt to find a line, line_coordinates contains first and last index of the pulsar
             line_coordinates = find_line(samples, start_sample_index, max_delay, pulsar_intensity)
-            
+
             # If a line is found, calculate and return the dispersion measure
-            if(line_coordinates is not None):
+            if line_coordinates is not None:
                 dm = line_coordinates[1] - line_coordinates[0]
                 return dm
 
@@ -63,39 +66,39 @@ def find_line(samples, start_sample_index, max_delay, pulsar_intensity):
     it stops if there is no intensity within the max_delay higher than the average_intensity
     '''
 
-    previous_sample_index = start_sample_index
+    previous_index = start_sample_index
 
     failed_to_find_line = True
 
     # Loop through the frequencies
-    for f, frequency in enumerate(samples[1]):
+    for f, _ in enumerate(samples[1]):
 
         # Loop through previous intensity until the max delay is reached
-        for i, intensity in enumerate(samples[:, f][previous_sample_index:previous_sample_index + max_delay]):
+        for i, intensity in enumerate(samples[:, f][previous_index:previous_index + max_delay]):
 
-            # Skip the first frequency, since that is where the initial sample is we are measuring from
-            if(f == 0):
+            # Skip the first frequency, since we start measuring from the first intensity
+            if f == 0:
                 failed_to_find_line = False
                 break
 
             # If the intensity is higher than the pulsar_intensity, continue finding a signal
-            if(intensity > pulsar_intensity):
-                previous_sample_index = previous_sample_index + i
+            if intensity > pulsar_intensity:
+                previous_index = previous_index + i
                 failed_to_find_line = False
                 break
 
         # If there is no line found, return None
-        if failed_to_find_line: 
+        if failed_to_find_line:
             return None
 
-    # If all frequencies are looped through, a line is found, so we return the start and end index of the line
-    return start_sample_index, previous_sample_index
-            
+    # If all frequencies are looped, a continuous signal is found,
+    # so we return the first and last index of the line
+    return start_sample_index, previous_index
 
 def find_estimation_intensity(samples, highest_x):
     '''
     This method finds the average intensity for the highest x intensities
-    The average_intensity is considered a requirement for intensities to be considered a pulsar 
+    The average_intensity is considered a requirement for intensities to be considered a pulsar
     '''
 
     # Sum of all intensities
