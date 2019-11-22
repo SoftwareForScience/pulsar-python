881d85e81a7f6e2d108eea93f57601be253319cb
JdeJong <jeroen--j@hotmail.com>
2019-11-22 12:22:53 +0100

    changed harmonic summing from script to module

---
+++ examples/sum_harmonics.py
@@ -0,0 +1,8 @@
+from harmonic_summing import harmonic_summing as harmsum
+import filterbank.filterbank as filterbank
+
+fb = filterbank.Filterbank(filename='./pspm32.fil', read_all=True)
+# get filertbank data + frequency labels
+fil_data = fb.select_data()
+
+harmsum.apply_harmonic_summing(fil_data=fil_data)
