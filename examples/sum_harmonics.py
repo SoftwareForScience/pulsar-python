388b7c956c8d89a7393aea7ac2c6fa0f90733fbc
Ankris <ventura.carriazo@gmail.com>
2019-11-28 16:32:07 +0100

    plot and watterfall

--- examples/sum_harmonics.py
+++ examples/sum_harmonics.py
@@ -1,9 +1,61 @@
-from harmonic_summing import harmonic_summing as harmsum
 import filterbank.filterbank as filterbank
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas import DataFrame
+from harmonic_summing import harmonic_summing as harmsum
+from plot import waterfall, waterfall_plot
+import pylab as pyl
 
 fb = filterbank.Filterbank(filename='./pspm32.fil', read_all=True)
 # get filertbank data + frequency labels
-fil_data = fb.select_data()
 
-harmsum.apply_harmonic_summing(fil_data=fil_data)
+freqs, fil_data = fb.select_data()
+
+
+harmsum = harmsum.apply_harmonic_summing(freqs, fil_data)
+print(harmsum)
+
+freqs, fil_data = harmsum
+
+
+# Create DataFrame with the fil_data changed and the freqs
+fil_dataframe = pd.DataFrame(fil_data, columns=freqs)
+
+list_num = [x for x in range(len(fil_data))]
+print(list_num)
+fil_dataframe['Sample'] = list_num
+
+# Get amplitudes of freq
+ampl = fil_dataframe.sum(axis=0, skipna=True)
+print(ampl.size)
+inten = []
+
+# Store in inten the amplitudes for each freq
+# -1 since dataframe has 'Sample' column wich we don't need here
+for i in range(ampl.size - 1):
+    inten.append(ampl[i])
+print(inten)
+
+# Create datatFrame with freq and their amplitudes
+ampl_freq = {'frequencies': freqs, 'Intensity': inten}
+a = DataFrame(ampl_freq, columns=['frequencies', 'Intensity'])
+print(a)
+
+# Plot the dataFrame
+a.plot(x='frequencies', y='Intensity', kind='line')
+plt.show()
+
+
+
+
+# Waterfall
+data, extent = waterfall_plot(fil_data, freqs)
+img = plt.imshow(data.T,
+                 aspect='auto',
+                 origin='lower',
+                 rasterized=True,
+                 interpolation='nearest',
+                 extent=extent,
+                 cmap='cubehelix')
+plt.show(img)
 
