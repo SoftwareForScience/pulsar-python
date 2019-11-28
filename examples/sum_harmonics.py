7f55ac6a299d483ef0bd49cac9ae83be296bce38
JdeJong <jeroen--j@hotmail.com>
2019-11-28 16:13:47 +0100

    refactored, cleaned, added parameters for harm sum

--- examples/sum_harmonics.py
+++ examples/sum_harmonics.py
@@ -3,7 +3,7 @@ import filterbank.filterbank as filterbank
 
 fb = filterbank.Filterbank(filename='./pspm32.fil', read_all=True)
 # get filertbank data + frequency labels
-fil_data = fb.select_data()
+freqs, fil_data = fb.select_data()
 
-harmsum.apply_harmonic_summing(fil_data=fil_data)
+harmsum.apply_harmonic_summing(frequencies=freqs, fil_data=fil_data, precision=0.001, num_lo_up_harmonics=(5, 5))
 
