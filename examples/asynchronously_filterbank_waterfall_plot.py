1a579f3216e9102415b7484b618474145067292e
Brian Lochan <brian.lochan@hva.nl>
2019-12-05 16:08:16 +0100

    made asynchronous test and fix

--- examples/asynchronously_filterbank_waterfall_plot.py
+++ examples/asynchronously_filterbank_waterfall_plot.py
@@ -20,32 +20,33 @@ async def create_waterfall(fb):
     wf = waterfall.Waterfall(filter_bank=fb, fig=pyl.figure(), mode='stream')
     await wf._init()
     wf.init_plot()
-
     return wf
 
-async def create_filterbank():
+async def create_filterbank(test_async=None):
     filterbank = Filterbank(filename='./pspm32.fil')
+
+    # just a async test to see if it works
+    if test_async is not None:
+        await asyncio.sleep(1)
+        print(test_async)
+    else:
+        print("1")
+
+
     # await filterbank._init()
 
     return filterbank
 
 async def main():
-    # fb = await create_filterbank()
-    fb = Filterbank(filename='./pspm32.fil')
+    # creates fb2 first and then fb1, because of sleep delay in create_filterbank method
+    fb1, fb2 = await asyncio.gather(create_filterbank("2"), create_filterbank())
+    wf1, wf2 = await asyncio.gather(create_waterfall(fb1), create_waterfall(fb2))
 
-    # wf = waterfall.Waterfall(filter_bank=fb, fig=pyl.figure(), mode='stream')
-    wf = await create_waterfall(fb)
-
-    # fig, update, frames, repeat = await wf.animated_plotter()
-
-    fig, update, frames, repeat = wf.animated_plotter()
+    fig, update, frames, repeat = wf1.animated_plotter()
 
     ani = animation.FuncAnimation(fig, update, frames=frames,repeat=repeat)
     plt.show(block=True)
 
-
 if __name__ == "__main__":
-    print(f"started at {time.strftime('%X')}")
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-    print(f"finished at {time.strftime('%X')}")
