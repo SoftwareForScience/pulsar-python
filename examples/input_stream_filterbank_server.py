4e935aa03bfd454aa485184a25daf0d379792048
Brian Lochan <brian.lochan@hva.nl>
2019-12-12 18:00:25 +0100

    Made server more user friendly and fixed remote connection via telnet

--- examples/input_stream_filterbank_server.py
+++ examples/input_stream_filterbank_server.py
@@ -4,10 +4,9 @@ import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
-import numpy as np
-import pickle
+import os
 
-async def print_filterbank_data(reader, writer):
+async def print_filterbank_data(reader, writer, not_connected=True):
     '''
     Reads and write data to the client. Also gets filterbank and gives data it contains back to the client.
     @param reader: reads input from sys.stdin
@@ -17,45 +16,62 @@ async def print_filterbank_data(reader, writer):
     @return: prints Filterbank data
     '''
 
-    data = await reader.read(100)
-    print(data)
-    message = data.decode().rstrip("\n\r")
-    addr = writer.get_extra_info('peername')
+    commands = "Type 'pspm32.fil' or 'pspm16.fil' or 'pspm8.fil' " \
+               "for filterbank data\nType '!quit' to close the connection\n"
 
-    # Instatiate the filterbank reader and point to the filterbank file
-    fb = Filterbank(filename="./"+message, read_all=True)
-
-    # read the data in the filterbank file
-    f, samples = fb.select_data()
-
-    # Get the powerlevels and the frequencies
-    for i in range(len(samples)):
-        # for j in range(len(samples[i])):
-        # print(samples[i][j])
-        writer.write((str(samples[i]).encode()))
+    if not_connected:
+        not_connected = False
+        print("device connected")
+        writer.write((("Welcome to the server!\n\n"+commands).encode()))
         await writer.drain()
-        # await asyncio.sleep(1)
 
-    print(f"Got file {message!r} from {addr!r}")
+    data = await reader.read(100)
 
-    # writer.write(data)
+    if data.decode().rstrip("\n\r") == "!quit":
+        print("Close the connection")
+        writer.close()
 
-    await print_filterbank_data(reader, writer)
+    else:
+        print("Received: " + data.decode())
+        message = data.decode().rstrip("\n\r")
 
-    # print("Close the connection")
-    # writer.close()
+        if message == "pspm32.fil" or message == "pspm16.fil" or message == "pspm8.fil":
+            addr = writer.get_extra_info('peername')
+
+            # Instatiate the filterbank reader and point to the filterbank file
+            fb = Filterbank(filename="./" + message, read_all=True)
+
+            # read the data in the filterbank file
+            f, samples = fb.select_data()
+
+            # Get the powerlevels and the frequencies
+            for i in range(len(samples)):
+                # for j in range(len(samples[i])):
+                # print(samples[i][j])
+                writer.write((str(samples[i]).encode()))
+                writer.write((str("\n\n").encode()))
+                await writer.drain()
+                # await asyncio.sleep(1)
+
+            print(f"Got file {message!r} from {addr!r}")
+
+        else:
+            writer.write(("'" + message + "'" + " is not accepted. \n" + commands).encode())
+
+        await print_filterbank_data(reader, writer, not_connected)
 
 async def main():
 
     '''
     Asynchronous Input stream server for filterbank data. Input is a filterbank filename without "./".
-    Output is printed filterbank data. When this server is running, go to commandline and type: telnet 127.0.0.1 8080
-    if it is connected then write filename like: pspm32.fil
+    Output is printed filterbank data. When this server is running, go to commandline and type:
+    telnet <local_ip> 12345 if it is connected then write filename like: pspm32.fil
     @return: Server on localhost
     '''
 
+    local_ip = os.popen("ipconfig getifaddr en0").read().strip('\n')
     server = await asyncio.start_server(
-        print_filterbank_data, '127.0.0.1', 8080)
+        print_filterbank_data, local_ip, 12345)
 
     addr = server.sockets[0].getsockname()
     print(f'Serving on {addr}')
